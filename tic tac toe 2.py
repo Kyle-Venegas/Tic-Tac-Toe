81aebce65f273c10efc4cd8e8830e544682a41a5
Kyle-Venegas <52269110+Kyle-Venegas@users.noreply.github.com>
2019-06-29 14:20:34 -0700

    Add files via upload

---
+++ tic tac toe 2.py
@@ -0,0 +1,85 @@
+grid = [
+        ["_", "_", "_"],
+        ["_", "_", "_"],
+        ["_", "_", "_"]
+        ]
+
+def who(turn):
+    if turn % 2 == 0:
+        return 2
+    else:
+        return 1
+
+def board():
+    print("    0    1    2")
+    y = 0
+    for row in grid:
+       print(y, row)   #after printing row, it goes to the next line
+       y += 1
+
+def playerLetter(player):
+    return "X" if player == 1 else "O"
+
+def makemove(player, row, col):
+    x = int(row)
+    y = int(col)
+    grid[x][y] = playerLetter(player)
+
+def CheckCol(x, player):
+    x = int(x)
+    for i in range(0, len(grid)):
+        if i == 0:
+            value = grid[i][x]
+        elif value != grid[i][x] or grid[i][x] == "_":
+            return False
+    return True
+
+def CheckRow(y, player):
+    y = int(y)
+    for i in range(0, len(grid)):
+        if i == 0:
+            value = grid[y][i]
+        elif value != grid[y][i] or grid[y][i] == "_":
+                return False
+    return True
+ 
+def victory2(player):
+    for i in range(3):
+        if CheckCol(i, player):
+            return True
+        if CheckRow(i, player):
+            return True
+    # left diagonal
+    if (grid[0][0] == grid[1][1] == grid[2][2] == playerLetter(player)):
+        return True
+    #right diagonal
+    if (grid[0][2] == grid[1][1] == grid[2][0] == playerLetter(player)):
+        return True
+    return False
+
+def check_(player, row, col):
+    row = int(row)
+    col = int(col)
+    if grid[row][col] == "X" or grid[row][col] == "O":
+        return True
+    return False
+
+import os
+
+turn = 1
+player = 1
+
+while True:
+    os.system("clear")
+    board() 
+    player = who(turn)
+    row = input("\nIn what row?: ")
+    col = input("In what column?: ")
+    if check_(player, row, col):
+        continue
+    makemove(player, row, col)
+    if victory2(player):
+        board()
+        print("\nPlayer {} wins".format(player)) 
+        break
+    turn += 1
