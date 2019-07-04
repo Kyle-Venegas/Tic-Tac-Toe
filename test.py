09eea4a84cf8d69556d385a848d240707d69b3b0
Kyle-Venegas <venegas.kyle@gmail.com>
2019-07-03 18:47:04 -0700

    Created board class and BOARD_SIZE constant

---
+++ test.py
@@ -0,0 +1,7 @@
+grid = [[" " for x in range(3)] for y in range(3)]
+
+def board():
+    for row in grid:
+        print(row)
+
+board()
--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,39 @@
 import os
 
-grid = [
-        ["_", "_", "_"],
-        ["_", "_", "_"],
-        ["_", "_", "_"]
-        ]
+BOARD_SIZE = 3
+
+class board:
+
+    def __init__(self):
+        self.grid = [[" " for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
+
+    def draw(self):
+        print("    0    1    2")
+        y = 0
+        for row in self.grid:
+           print(y, row)
+           y += 1
+ 
+    def makemove(self, player, row, col):
+        x = int(row)
+        y = int(col)
+        self.grid[x][y] = playerLetter(player)
+ 
+    def victory(self, player):
+        for i in range(BOARD_SIZE):
+            if CheckCol(i, player):
+                return True
+            if CheckRow(i, player):
+                return True
+        # left diagonal
+        if (self.grid[0][0] == self.grid[1][1] == self.grid[2][2] == playerLetter(player)):
+            return True
+        #right diagonal
+        if (self.grid[0][2] == self.grid[1][1] == self.grid[2][0] == playerLetter(player)):
+            return True
+        return False
+
+game = board()
 
 def who(turn):
     if turn % 2 == 0:
@@ -12,57 +41,33 @@ def who(turn):
     else:
         return 1
 
-def board():
-    print("    0    1    2")
-    y = 0
-    for row in grid:
-       print(y, row)   #after printing row, it goes to the next line
-       y += 1
-
 def playerLetter(player):
     return "X" if player == 1 else "O"
 
-def makemove(player, row, col):
-    x = int(row)
-    y = int(col)
-    grid[x][y] = playerLetter(player)
 
 def CheckCol(x, player):
     x = int(x)
-    for i in range(0, len(grid)):
+    for i in range(0, len(game.grid)):
         if i == 0:
-            value = grid[i][x]
-        elif value != grid[i][x] or grid[i][x] == "_":
+            value = game.grid[i][x]
+        elif value != game.grid[i][x] or game.grid[i][x] == "_":
             return False
     return True
 
 def CheckRow(y, player):
     y = int(y)
-    for i in range(0, len(grid)):
+    for i in range(0, len(game.grid)):
         if i == 0:
-            value = grid[y][i]
-        elif value != grid[y][i] or grid[y][i] == "_":
+            value = game.grid[y][i]
+        elif value != game.grid[y][i] or game.grid[y][i] == "_":
                 return False
     return True
 
-def victory2(player):
-    for i in range(3):
-        if CheckCol(i, player):
-            return True
-        if CheckRow(i, player):
-            return True
-    # left diagonal
-    if (grid[0][0] == grid[1][1] == grid[2][2] == playerLetter(player)):
-        return True
-    #right diagonal
-    if (grid[0][2] == grid[1][1] == grid[2][0] == playerLetter(player)):
-        return True
-    return False
 
 def check_(player, row, col):
     row = int(row)
     col = int(col)
-    if grid[row][col] == "X" or grid[row][col] == "O":
+    if game.grid[row][col] == "X" or game.grid[row][col] == "O":
         return True
     return False
 
@@ -71,15 +76,15 @@ def playTicTacToe():
     player = 1
     while True:
         os.system("clear")
-        board()
+        game.draw()
         player = who(turn)
         row = input("\nIn what row?: ")
         col = input("In what column?: ")
         if check_(player, row, col):
             continue
-        makemove(player, row, col)
-        if victory2(player):
-            board()
+        game.makemove(player, row, col)
+        if game.victory(player):
+            game.draw()
             print("\nPlayer {} wins".format(player))
             break
         turn += 1
